21c551928d1aefbf58156f7acedab08ee056a751
mbdorado <82581830+mbdorado@users.noreply.github.com>
2021-12-06 03:52:23 +0800

    location updates on rider's interface

--- CS4_Proto/app/src/main/java/com/cs4/cs4_proto1/rider/MapLocation.java
+++ CS4_Proto/app/src/main/java/com/cs4/cs4_proto1/rider/MapLocation.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,21 +24,23 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MapLocation extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int PERMISSION_FINE_LOCATION = 99;
-    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int DEFAULT_UPDATE_INTERVAL = 20;
     public static final int FASTEST_UPDATE_INTERVAL = 5;
     private GoogleMap mMap;
     private ActivityMapLocationBinding binding;
 
     FusedLocationProviderClient fusedLocationProviderClient;
-    LocationCallback locationCallback;
+    LocationRequest locationRequest;
+
+    Marker riderMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +53,24 @@ public class MapLocation extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest
+                .setInterval(1000 * DEFAULT_UPDATE_INTERVAL)
+                .setFastestInterval(1000 * FASTEST_UPDATE_INTERVAL)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch(requestCode){
+        switch (requestCode) {
             case PERMISSION_FINE_LOCATION:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    updateLocation();
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates();
                 } else {
                     Toast.makeText(this, "This app requires permission to be granted in order to work properly.", Toast.LENGTH_SHORT).show();
                     finish();
@@ -71,48 +82,70 @@ public class MapLocation extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LocationRequest.create()
-                .setInterval(1000 * DEFAULT_UPDATE_INTERVAL)
-                .setFastestInterval(1000 * FASTEST_UPDATE_INTERVAL)
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        updateLocation();
-
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(@NonNull LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-
-
-            }
-        };
-    }
-
-    private void updateLocation() {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapLocation.this);
-
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             //Permission Granted
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    LatLng riderLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(riderLocation).title("You"));
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(riderLocation, 20));
-                    updateRealtimeDatabase();
-                }
-            });
+            startLocationUpdates();
         } else {
             //Permission Denied
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
             }
         }
-
     }
 
-    private void updateRealtimeDatabase() {
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            if(mMap != null) {
+                updateMapLocation(locationResult.getLastLocation());
+            }
+        }
+    };
+    private void startLocationUpdates() {
+        //continuous update of rider's location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+            }
+            return;
+        }
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void updateMapLocation(Location location) {
+        LatLng riderLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        if(riderMarker == null) {
+            //create new marker
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(riderLocation);
+            riderMarker = mMap.addMarker(markerOptions);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(riderLocation, 20));
+        } else {
+            //use the previously created marker
+            riderMarker.setPosition(riderLocation);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(riderLocation, 20));
+        }
+    }
+
+    private void updateRealtimeDatabase(Location location) {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference("users");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+    }
 }
